33bb5f241a2d5f0585f7b0a47b14b2295ce7c789
david <david@Nitishs-MacBook-Air-501.local>
2024-06-19 15:46:36 +0530

    UC2 add new contacts to address book

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookSystem {
     private String firstName;
     private String lastNames;
@@ -7,6 +10,8 @@ public class AddressBookSystem {
     private int zip;
     private long phoneNumber;
     private String email;
+    static ArrayList<AddressBookSystem> contacts=new ArrayList<>();
+    static Scanner sc=new Scanner(System.in);
     AddressBookSystem(String firstName,String lastNames,String address,String city,String state,int zip,long phoneNumber,String email){
         this.firstName=firstName;
         this.lastNames=lastNames;
@@ -17,8 +22,47 @@ public class AddressBookSystem {
         this.phoneNumber=phoneNumber;
         this.email=email;
     }
+    @Override
+    public String toString() {
+        return "FirstName: "+firstName + "\nLastName: " + lastNames + "\nAddress: "+address+"\nCity: "+city+"\nState: "+state+"\nZIP: "+zip+"\nPhone Number: "+phoneNumber+"\nEmail: "+email;
+
+    }
+    static void print(){
+        for(AddressBookSystem contact: contacts){
+            System.out.println(contact);
+        }
+    }
+    private static void addContacts(){
+        System.out.println("Enter contact details");
+        System.out.println("Enter first name:");
+        String fn= sc.next();
+        System.out.println("Enter last name:");
+        String ln= sc.next();
+        System.out.println("Enter address:");
+        String add= sc.nextLine();
+        System.out.println("Enter city:");
+        String city= sc.next();
+        System.out.println("Enter state:");
+        String state= sc.next();
+        System.out.println("Enter zip:");
+        int zip= sc.nextInt();
+        System.out.println("Enter PhoneNo.:");
+        long phn= sc.nextLong();
+        System.out.println("Enter email:");
+        String email= sc.next();
+        contacts.add(new AddressBookSystem(fn,ln,add,city,state,zip,phn,email));
+    }
     public static void main(String[] args) {
+
         System.out.println("Welcome to Address Book System");
-        AddressBookSystem Contact1=new AddressBookSystem("Aman","Singh","12/A Malhotra Heights","Mumbai","Maharastra",208037,9876543210L,"amam@gmail.com");
+//        AddressBookSystem Contact1=new AddressBookSystem("Aman","Singh","12/A Malhotra Heights","Mumbai","Maharastra",208037,9876543210L,"amam@gmail.com");
+        ArrayList<AddressBookSystem> contacts=new ArrayList<>();
+        System.out.println("Enter number of contacts");
+        int n= sc.nextInt();
+        for (int i=0;i<n;i++){
+            addContacts();
+        }
+        print();
+
     }
 }
