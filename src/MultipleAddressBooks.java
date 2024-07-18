3c19d806141b2e2b40dd00620ab6484a59fbc590
david <david@Nitishs-MacBook-Air-501.local>
2024-07-18 10:53:39 +0530

    Added: Sort entries in address book by city, state, zip

--- src/MultipleAddressBooks.java
+++ src/MultipleAddressBooks.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class MultipleAddressBooks {
-    private HashMap<String, AddressBookSystem> addressBooks = new HashMap<>();
+    private static HashMap<String, AddressBookSystem> addressBooks = new HashMap<>();
     private static HashMap<String, List<AddressBookSystem>> cityToPersons = new HashMap<>();
     private HashMap<String, List<AddressBookSystem>> stateToPersons = new HashMap<>();
     private static Scanner sc = new Scanner(System.in);
@@ -25,6 +25,59 @@ public class MultipleAddressBooks {
             cityToPersons.computeIfAbsent(contact.getCity(), k -> new ArrayList<>()).add(contact);
             stateToPersons.computeIfAbsent(contact.getState(), k -> new ArrayList<>()).add(contact);
         }
+    }
+    private static void printEntriesSortedByState() {
+        System.out.println("Entries sorted by State:");
+        addressBooks.values().stream()
+                .flatMap(book -> book.getContacts().stream())
+                .sorted((c1, c2) -> {
+                    int stateComparison = c1.getState().compareToIgnoreCase(c2.getState());
+                    if (stateComparison != 0) {
+                        return stateComparison;
+                    } else {
+                        return c1.getFirstName().compareToIgnoreCase(c2.getFirstName());
+                    }
+                })
+                .forEach(contact -> {
+                    System.out.println(contact);
+                    System.out.println();
+                });
+    }
+    private static void printEntriesSortedByCity(){
+        System.out.println("Entries sorted by State:");
+        addressBooks.values().stream()
+                .flatMap(book-> book.getContacts().stream())
+                .sorted((c1,c2)->{
+                    int cityComparison =c1.getCity().compareToIgnoreCase(c2.getCity());
+                    if (cityComparison!=0){
+                        return cityComparison;
+                    }
+                    else{
+                        return c1.getFirstName().compareToIgnoreCase(c2.getFirstName());
+                    }
+                })
+                .forEach(contact ->{
+                    System.out.println(contact);
+                    System.out.println();
+                });
+    }
+    private static void printEntriesSortedByZip(){
+        System.out.println("Entries sorted by State:");
+        addressBooks.values().stream()
+                .flatMap(book-> book.getContacts().stream())
+                .sorted((c1,c2)->{
+                    int zipComparison =c1.getZip().compareToIgnoreCase(c2.getZip());
+                    if (zipComparison!=0){
+                        return zipComparison;
+                    }
+                    else{
+                        return c1.getFirstName().compareToIgnoreCase(c2.getFirstName());
+                    }
+                })
+                .forEach(contact ->{
+                    System.out.println(contact);
+                    System.out.println();
+                });
     }
     private void searchInCity(String cityName) {
         System.out.println("Search Results in City \"" + cityName + "\":");
@@ -50,7 +103,7 @@ public class MultipleAddressBooks {
 
         boolean check = true;
         while (check) {
-            System.out.println("Enter 1 to Add Address Book\nEnter 2 to Search by City\nEnter 3 to Search by State\nEnter 4 to Count by City\nEnter 5 to Count by State\nEnter 6 to Exit");
+            System.out.println("Enter 1 to Add Address Book\nEnter 2 to Search by City\nEnter 3 to Search by State\nEnter 4 to Count by City\nEnter 5 to Count by State\nEnter 6 to sort by City\nEnter 6 to sort by State\nEnter 6 to sort by Zip\nEnter 9 to Exit");
             int choice = sc.nextInt();
             sc.nextLine();
 
@@ -81,6 +134,12 @@ public class MultipleAddressBooks {
                     System.out.print("Number of contacts found in "+ stateCount +": "+cityToPersons.getOrDefault(stateCount, new ArrayList<>()).size() );
                     break;
                 case 6:
+                    printEntriesSortedByCity();
+                case 7:
+                    printEntriesSortedByState();
+                case 8:
+                    printEntriesSortedByZip();
+                case 9:
                     check = false;
                     break;
                 default:
